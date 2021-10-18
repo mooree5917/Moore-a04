2fd386d7db58ff8f996db1c121d2362fed89e077
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-17 23:55:34 -0400

    exercise43

--- exercise43/src/main/java/baseline/solution43.java
+++ exercise43/src/main/java/baseline/solution43.java
@@ -1,2 +1,20 @@
-package baseline;public class solution43 {
+/*
+ UCF COP3330 Fall 2021 Assignment Solutions
+ Copyright 2021 Eric Moore
+ Programming languages can create files and folders too.
+ Create a program that generates a website skeleton with the following specifications:
+
+ Prompt for the name of the site.
+ Prompt for the author of the site.
+ Ask if the user wants a folder for JavaScript files.
+ Ask if the user wants a folder for CSS files.
+ Generate an index.html file that contains the name of the site inside the <title> tag and the author in a <meta> tag.
+ The user should then find these files and directories created in the working directory of your program.
+ */
+package baseline;
+
+public class solution43 {
+    public static void main(String[] args) {
+
+    }
 }
