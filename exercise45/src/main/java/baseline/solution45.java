95ecc9741edd03407b3d7c8a7f4f8ceaa566b263
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-17 23:56:54 -0400

    exercise 45

--- exercise45/src/main/java/baseline/solution45.java
+++ exercise45/src/main/java/baseline/solution45.java
@@ -1,2 +1,17 @@
-package baseline;public class solution45 {
+/*
+UCF COP3330 Fall 2021 Assignment Solutions
+Copyright 2021 Eric Moore
+There will be times when you'll need to read in one file, modify it,
+and then write a modified version of that file to a new file.
+Given an input file named `exercise45_input.txt`, read the file and look for all occurrences of the word utilize.
+Replace each occurrence with use. Write the modified file to a new file.
+Prompt for the name of the output file.
+Write the output to a new file.
+*/
+package baseline;
+
+public class solution45 {
+    public static void main(String[] args) {
+
+    }
 }
