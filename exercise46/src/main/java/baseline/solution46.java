55020ce00660497b1e7502b9631575279301180e
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-17 23:54:17 -0400

    added data and diagram

--- exercise46/src/main/java/baseline/solution46.java
+++ exercise46/src/main/java/baseline/solution46.java
@@ -1,2 +1,18 @@
-package baseline;public class solution46 {
+/*
+UCF COP3330 Fall 2021 Assignment Solutions
+Copyright 2021 Eric Moore
+Knowing how often a word appears in a sentence or block of text is helpful for
+creating word clouds and other types of word analysis.
+And it’s more useful when running it against lots of text.
+Create a program that reads in a file named `exercise46_input.txt` and
+counts the frequency of words in the file.
+Then construct a histogram displaying the words and the frequency, and display the histogram to the screen.
+Ensure that the most used word is at the top of the report and the least used words are at the bottom.
+ */
+package baseline;
+
+public class solution46 {
+    public static void main(String[] args) {
+
+    }
 }
