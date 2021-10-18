46b73352fb36989f3b8eff2f7c7b343ae4bb2154
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-17 23:56:15 -0400

    exercise 44

--- exercise44/src/main/java/baseline/solution44.java
+++ exercise44/src/main/java/baseline/solution44.java
@@ -1,2 +1,21 @@
-package baseline;public class solution44 {
+/*
+ UCF COP3330 Fall 2021 Assignment Solutions
+ Copyright 2021 Eric Moore
+ Pulling data from a file into a complex data structure makes parsing much simpler.
+ Many programming languages support the JSON format, a popular way of representing data.
+ Create a program that takes a product name as input and retrieves the current price and quantity for that product.
+ The product data is in a data file in the JSON format  and looks like this
+ (you will create this file as `exercise44_input.json`):
+ Print out the product name, price, and quantity if the product is found.
+ If no product matches the search, state that no product was found and start over.
+ The file is in the JSON format. Use a JSON parser to pull the values out of the file
+ (e.g. https://github.com/google/gson (Links to an external site.)).
+ If no record is found, prompt again.
+ */
+package baseline;
+
+public class solution44 {
+    public static void main(String[] args) {
+
+    }
 }
